b5c8f755236e538b6aab6e306d09c726e651cac6
BlayneturnerT16 <diablo3wizardforstray@gmail.com>
2020-12-24 09:31:11 -0800

    Please work please its all i have left

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UrsaBot/testOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/UrsaBot/testOpMode.java
@@ -4,8 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-//commit
-
 @TeleOp(name="TestOpMode")
 public class testOpMode extends LinearOpMode {
 
